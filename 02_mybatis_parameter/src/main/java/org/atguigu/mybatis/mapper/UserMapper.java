548a61c4f8b39aaec7be4a2a142c6e78fdd17098
liangjinyong <liangjy881@gmail.com>
2025-06-17 15:36:32 +0900

    18 使用模板搭建MyBatis框架

---
+++ 02_mybatis_parameter/src/main/java/org/atguigu/mybatis/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package org.atguigu.mybatis.mapper;
+
+/**
+ * ClassName: UserMapper
+ * Package: org.atguigu.mybatis.mapper
+ * Description:
+ *
+ * @Author: ljy
+ * @Create: 2025. 6. 17. 오후 3:27
+ * @Version 1.0
+ */
+public interface UserMapper {
+}
