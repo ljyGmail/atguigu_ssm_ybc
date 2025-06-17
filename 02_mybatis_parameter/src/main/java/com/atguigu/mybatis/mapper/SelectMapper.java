686b5d5e54c0ac28a1fabd6bea141c4294b7e342
ljyGmail <liangjy881@gmail.com>
2025-06-17 22:25:33 +0900

    25 MyBatis的各种查询功能(1)

---
+++ 02_mybatis_parameter/src/main/java/com/atguigu/mybatis/mapper/SelectMapper.java
@@ -0,0 +1,30 @@
+package com.atguigu.mybatis.mapper;
+
+import com.atguigu.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SelectMapper {
+
+    /**
+     * 若sql语句查询的结果为多条时，一定不能以实体类类型作为方法的返回值
+     * 否则会抛出异常: TooManyResultsException
+     * 若sql语句查询的结果为一条时，此时可以使用实体类类型或list集合类型作为方法的返回值
+     */
+
+    /**
+     * 根据id查询用户信息
+     *
+     * @param id
+     * @return
+     */
+    User getUserById(@Param("id") Integer id);
+
+    /**
+     * 查询所有的用户信息
+     *
+     * @return
+     */
+    List<User> getAllUsers();
+}
