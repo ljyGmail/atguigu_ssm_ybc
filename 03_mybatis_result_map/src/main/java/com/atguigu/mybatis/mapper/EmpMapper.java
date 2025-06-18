3fc10d56c0a3db275702712e965379e675af0cb3
liangjinyong <liangjy881@gmail.com>
2025-06-18 16:57:19 +0900

    35 使用全局配置处理字段名和属性名不一致的情况

--- 03_mybatis_result_map/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
+++ 03_mybatis_result_map/src/main/java/com/atguigu/mybatis/mapper/EmpMapper.java
@@ -1,5 +1,8 @@
 package com.atguigu.mybatis.mapper;
 
+import com.atguigu.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
+
 /**
  * ClassName: EmpMapper
  * Package: com.atguigu.mybatis.mapper
@@ -11,4 +14,11 @@ package com.atguigu.mybatis.mapper;
  */
 public interface EmpMapper {
 
+    /**
+     * 根据id查询员工信息
+     *
+     * @param empId
+     * @return
+     */
+    Emp getEmpByEmpId(@Param("empId") Integer empId);
 }
---
+++ 03_mybatis_result_map/src/test/java/com/atguigu/mybatis/ResultMapTest.java
@@ -0,0 +1,29 @@
+package com.atguigu.mybatis;
+
+import com.atguigu.mybatis.mapper.EmpMapper;
+import com.atguigu.mybatis.pojo.Emp;
+import com.atguigu.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.EnumMap;
+
+/**
+ * ClassName: ResultMapTest
+ * Package: com.atguigu.mybatis
+ * Description:
+ *
+ * @Author: ljy
+ * @Create: 2025. 6. 18. 오후 4:42
+ * @Version 1.0
+ */
+public class ResultMapTest {
+
+    @Test
+    public void testGetEmpByEmpId() {
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        Emp emp = mapper.getEmpByEmpId(1);
+        System.out.println("emp: " + emp);
+    }
+}
