3e6234afef8300496cfb4ae50b16d7541e46ee6c
liangjinyong <liangjy881@gmail.com>
2025-06-23 10:10:45 +0900

    77 依赖注入之p命名空间

--- 08_spring_ioc_xml/src/test/java/com/atguigu/spring/test/IOCByXMLTest.java
+++ 08_spring_ioc_xml/src/test/java/com/atguigu/spring/test/IOCByXMLTest.java
@@ -57,13 +57,16 @@ public class IOCByXMLTest {
         // System.out.println("student: " + student);
         // Student student = ioc.getBean("studentFour", Student.class);
         // System.out.println("student: " + student);
-        Student student = ioc.getBean("studentFive", Student.class);
-        System.out.println("student: " + student);
+        // Student student = ioc.getBean("studentFive", Student.class);
+        // System.out.println("student: " + student);
 
         // Clazz clazz = ioc.getBean("clazzInner", Clazz.class);
         // System.out.println("class: " + clazz);
 
         // Clazz clazz = ioc.getBean("clazzOne", Clazz.class);
         // System.out.println("class: " + clazz);
+
+        Student student = ioc.getBean("studentSix", Student.class);
+        System.out.println("student: " + student);
     }
 }
