523d67a7d4545f1995d6824abd84eeb42cafd8ba
liangjinyong <liangjy881@gmail.com>
2025-06-27 18:16:12 +0900

    93 代理模式之场景模拟

---
+++ 10_spring_proxy/src/main/java/com/atguigu/spring/proxy/Calculator.java
@@ -0,0 +1,21 @@
+package com.atguigu.spring.proxy;
+
+/**
+ * ClassName: Calculator
+ * Package: com.atguigu.spring.proxy
+ * Description:
+ *
+ * @Author: ljy
+ * @Create: 2025. 6. 27. 오후 6:08
+ * @Version 1.0
+ */
+public interface Calculator {
+
+    int add(int i, int j);
+
+    int sub(int i, int j);
+
+    int mul(int i, int j);
+
+    int div(int i, int j);
+}
