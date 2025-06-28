7ad9cff06cc24436681bfdc0f6f89f3d3dffc904
liangjinyong <liangjy881@gmail.com>
2025-06-28 15:24:13 +0900

    99 基于注解的AOP之准备工作

---
+++ 11_spring_aop/src/main/java/com/atguigu/spring/aop/annotation/LoggerAspect.java
@@ -0,0 +1,18 @@
+package com.atguigu.spring.aop.annotation;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * ClassName: LoggerAspect
+ * Package: com.atguigu.spring.aop.annotation
+ * Description:
+ *
+ * @Author: ljy
+ * @Create: 2025. 6. 28. 오후 3:19
+ * @Version 1.0
+ */
+@Component
+@Aspect // 将当前组件标识为切面
+public class LoggerAspect {
+}
