b48ce4c8a59b8fd66ff443fc76956cdcfe75d375
liangjinyong <liangjy881@gmail.com>
2025-06-29 14:42:54 +0900

    100 基于注解的AOP之前置通知

--- 11_spring_aop/src/main/java/com/atguigu/spring/aop/annotation/LoggerAspect.java
+++ 11_spring_aop/src/main/java/com/atguigu/spring/aop/annotation/LoggerAspect.java
@@ -1,13 +1,16 @@
 package com.atguigu.spring.aop.annotation;
 
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /**
  * ClassName: LoggerAspect
  * Package: com.atguigu.spring.aop.annotation
  * Description:
+ * 在切面中，需要通过指定的注解将方法标识为通知方法
  *
+ * @Before: 前置通知，在目标对象方法执行之前执行
  * @Author: ljy
  * @Create: 2025. 6. 28. 오후 3:19
  * @Version 1.0
@@ -15,4 +18,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect // 将当前组件标识为切面
 public class LoggerAspect {
+
+    @Before("execution(public int com.atguigu.spring.aop.annotation.CalculatorImpl.add(int, int))")
+    public void beforeAdviceMethod() {
+        System.out.println("LoggerAspect, 前置通知");
+    }
 }
---
+++ 11_spring_aop/src/test/java/com/atguigu/spring/test/AOPTest.java
@@ -0,0 +1,28 @@
+package com.atguigu.spring.test;
+
+import com.atguigu.spring.aop.annotation.Calculator;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * ClassName: AOPTest
+ * Package: com.atguigu.spring.test
+ * Description:
+ *
+ * @Author liangjinyong
+ * @Create 2025-06-29 오후 2:35
+ * @Version 1.0
+ */
+public class AOPTest {
+
+    @Test
+    public void testAOPByAnnotation() {
+        ApplicationContext ioc = new ClassPathXmlApplicationContext("aop-annotation.xml");
+        // 无法直接获取目标对象
+        // CalculatorImpl calculator = ioc.getBean(CalculatorImpl.class);
+        // calculator.add(1, 2);
+        Calculator calculator = ioc.getBean(Calculator.class);
+        calculator.add(1, 2);
+    }
+}
