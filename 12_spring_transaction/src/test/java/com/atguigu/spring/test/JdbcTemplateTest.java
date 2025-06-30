874279fd2c9f3e24a6d47aac62480cbebe42629f
liangjinyong <liangjy881@gmail.com>
2025-06-30 23:51:03 +0900

    108 JdbcTemplate实现查询功能

--- 12_spring_transaction/src/test/java/com/atguigu/spring/test/JdbcTemplateTest.java
+++ 12_spring_transaction/src/test/java/com/atguigu/spring/test/JdbcTemplateTest.java
@@ -1,11 +1,15 @@
 package com.atguigu.spring.test;
 
+import com.atguigu.spring.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 /**
  * ClassName: JdbcTemplateTest
@@ -29,5 +33,26 @@ public class JdbcTemplateTest {
     public void testInsert() {
         String sql = "insert into t_user values(null, ?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql, "root", "445566", 26, "女", "123@qq.com");
+    }
+
+    @Test
+    public void testGetUserById() {
+        String sql = "select * from t_user where id = ?";
+        User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), 1);
+        System.out.println("user: " + user);
+    }
+
+    @Test
+    public void getAllUsers() {
+        String sql = "select * from t_user";
+        List<User> list = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        list.forEach(System.out::println);
+    }
+
+    @Test
+    public void getGetCount() {
+        String sql = "select count(*) from t_user";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+        System.out.println("count: " + count);
     }
 }
