da88341525f8ebe23f0f00e2bc1f376fff3635c8
liangjinyong <liangjy881@gmail.com>
2025-07-02 11:35:10 +0900

    113 声明式事务的属性之只读、超时、回滚策略

--- 12_spring_transaction/src/main/java/com/atguigu/spring/service/impl/BookServiceImpl.java
+++ 12_spring_transaction/src/main/java/com/atguigu/spring/service/impl/BookServiceImpl.java
@@ -21,14 +21,28 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private BookDao bookDao;
 
-    @Transactional
+    @Transactional(
+            // readOnly = true,
+            // timeout = 3,
+            // noRollbackFor = ArithmeticException.class,
+            // noRollbackForClassName = "java.lang.ArithmeticException"
+    )
     @Override
     public void buyBook(Integer userId, Integer bookId) {
+        /*
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         */
+
         // 查询图书的价格
         Integer price = bookDao.getPriceByBookId(bookId);
         // 更新图书的库存
         bookDao.updateStock(bookId);
         // 更新用户的余额
         bookDao.updateBalance(userId, price);
+        // System.out.println(1 / 0);
     }
 }
